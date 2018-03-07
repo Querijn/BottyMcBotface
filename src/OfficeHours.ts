5b09a7d2dcb2584759c21b0a2b47520f9b8b32b9
stelar7 <dudedbz@gmail.com>
2018-03-07 11:07:57 +0100

    Inline variables
    Change question uuid from string to number
    Use const instead of var in loops
    Use === instead of == for comparison
    Use = [] to empty array instead of .splice
    Use .some(.has) instead of .some(!!.get)

--- src/OfficeHours.ts
+++ src/OfficeHours.ts
@@ -4,7 +4,7 @@ import { fileBackedObject } from "./FileBackedObject";
 import Discord = require("discord.js");
 
 interface QuestionData {
-    uuid: string;
+    uuid: number;
     author: string;
     question: string;
 }
@@ -12,12 +12,8 @@ interface QuestionData {
 export default class OfficeHours {
     private bot: Discord.Client;
     private questions: QuestionData[];
-    private adminRoles: string[];
+    private sharedSettings: SharedSettings;
 
-    private openMessage: string;
-    private closeMessage: string;
-    private addedMessage: string;
-    private removedMessage: string;
 
     constructor(bot: Discord.Client, sharedSettings: SharedSettings, questionFile: string) {
         console.log("Requested OfficeHours extension..");
@@ -26,11 +22,6 @@ export default class OfficeHours {
         this.questions = fileBackedObject(questionFile);
         console.log("Successfully question file.");
 
-        this.openMessage = sharedSettings.officehours.openMessage;
-        this.closeMessage = sharedSettings.officehours.closeMessage;
-        this.addedMessage = sharedSettings.officehours.addedMessage;
-        this.removedMessage = sharedSettings.officehours.removedMessage;
-        this.adminRoles = sharedSettings.officehours.allowedRoles;
 
         this.bot.on("ready", this.onBot.bind(this));
         this.bot.on("message", this.onCommand.bind(this));
@@ -41,14 +32,14 @@ export default class OfficeHours {
     }
 
     nextId() {
-        return Math.max.apply(Math, this.questions.map(o => o.uuid)) + 1;
+        return Math.max(...this.questions.map(x => x.uuid)) + 1;
     }
 
 
     onCommand(message: Discord.Message) {
 
         const authorId = message.author.id;
-        const isAdmin = (message.member && findOne(message.member.roles, this.adminRoles));
+        const isAdmin = (message.member && findOne(message.member.roles, this.sharedSettings.officehours.allowedRoles));
 
         if (message.content.startsWith("!ask")) {
 
@@ -61,7 +52,7 @@ export default class OfficeHours {
 
             this.questions.push(qData);
 
-            message.reply(this.addedMessage);
+            message.reply(this.sharedSettings.officehours.addedMessage);
         }
 
         if (!isAdmin) {
@@ -70,7 +61,7 @@ export default class OfficeHours {
 
         if (message.content.startsWith("!question_list")) {
 
-            for (var data of this.questions) {
+            for (const data of this.questions) {
                 message.author.sendMessage(`${data.uuid}: <@${data.author}>: ${data.question}`)
             }
         }
@@ -78,35 +69,33 @@ export default class OfficeHours {
         if (message.content.startsWith("!question_remove")) {
             const arr = message.content.split(" ");
 
-            if (arr.length == 2) {
-                const id = message.content.split(" ")[1];
+            if (arr.length === 2) {
+                const id = Number(message.content.split(" ")[1]);
                 this.questions = this.questions.filter(q => q.uuid != id);
             }
 
-            message.reply(this.removedMessage);
+            message.reply(this.sharedSettings.officehours.removedMessage);
         }
 
         if (message.content.startsWith("!open")) {
             message.delete();
-            message.channel.sendMessage(this.openMessage);
+            message.channel.sendMessage(this.sharedSettings.officehours.openMessage);
 
-            for (var data of this.questions) {
+            for (const data of this.questions) {
                 message.channel.sendMessage(`<@${data.author}>: ${data.question}`)
             }
 
-            this.questions.splice(0, this.questions.length);
+            this.questions = [];
         }
 
         if (message.content.startsWith("!close")) {
             message.delete();
-            message.channel.sendMessage(this.closeMessage);
+            message.channel.sendMessage(this.sharedSettings.officehours.closeMessage);
         }
     }
 }
 
 
 const findOne = (arr1: Discord.Collection<string, Discord.Role>, arr2: Array<any>) => {
-    return arr2.some(v => {
-        return !!arr1.get(v);
-    });
+    return arr2.some(x => arr1.has(x));
 };
