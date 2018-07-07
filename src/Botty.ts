8a674e5bc3e115204fbbbeeacfda8fdf6e86986b
Querijn Heijmans <querijn@irule.at>
2018-07-07 11:42:48 +0200

    Remove non-functional updates

--- src/Botty.ts
+++ src/Botty.ts
@@ -75,26 +75,9 @@ export default class Botty {
                 console.log(`${oldMember.nickname} changed his nickname to ${newMember.nickname}.`);
             }
 
-            if (oldMember.user.avatarURL !== newMember.user.avatarURL) {
-                console.log(`${oldMember.displayName} changed his avatar from ${oldMember.user.avatarURL} to ${newMember.user.avatarURL}.`);
-            }
-
             if (oldMember.user.discriminator !== newMember.user.discriminator) {
                 console.log(`${oldMember.displayName} changed his discriminator from ${oldMember.user.discriminator} to ${newMember.user.discriminator}.`);
             }
-
-            const oldGame = oldMember.user.presence && oldMember.user.presence.game ? oldMember.user.presence.game.name : "nothing";
-            const newGame = newMember.user.presence && newMember.user.presence.game ? newMember.user.presence.game.name : "nothing";
-            if (oldGame !== newGame) {
-                console.log(`${oldMember.displayName} is now playing ${newGame} (was ${oldGame}).`);
-            }
-
-            const oldStatus = (oldMember.user.presence && oldMember.user.presence.status) ? oldMember.user.presence.status : "offline (undefined)";
-            const newStatus = (newMember.user.presence && newMember.user.presence.status) ? newMember.user.presence.status : "offline (undefined)";
-            if (oldStatus !== newStatus && (newStatus === "offline" || newStatus === "online")) {
-                console.log(`${oldMember.displayName} is now ${newStatus} (was ${oldStatus}).`);
-            }
-
         });
         console.log("Initialised listeners.");
     }
