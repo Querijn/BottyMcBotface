8de5495f2e86bc53acb7d7884e542f105f600f63
Querijn Heijmans <querijn@irule.at>
2018-07-10 12:57:40 +0200

    Reorder app modules

--- src/app.ts
+++ src/app.ts
@@ -26,32 +26,43 @@ const commandList = fileBackedObject<CommandList>("settings/command_list.json");
 const bot = new Botty(sharedSettings);
 
 // Load extensions
-const apiSchema = new APISchema(sharedSettings);
 const controller = new CommandController(bot.client, sharedSettings, "data/command_data.json");
+const apiSchema = new APISchema(sharedSettings);
 const joinMessaging = new JoinMessaging(bot.client, sharedSettings, controller);
 const logger = new Logger(bot.client, sharedSettings);
 const keyFinder = new KeyFinder(bot.client, sharedSettings, "data/riot_keys.json");
 const forum = new ForumReader(bot.client, sharedSettings, "data/forum_data.json", keyFinder);
 const techblog = new Techblog(bot.client, sharedSettings, "data/techblog_data.json");
 const apiUrlInterpreter = new ApiUrlInterpreter(bot.client, sharedSettings, apiSchema);
+const versionChecker = new VersionChecker(bot.client, sharedSettings, "data/version_data.json");
+const notes = new Info(bot, sharedSettings, "data/info_data.json", versionChecker);
+const officeHours = new OfficeHours(bot.client, sharedSettings, "data/office_hours_data.json");
+const react = new AutoReact(bot.client, sharedSettings, "data/thinking_data.json", "data/ignored_react_data.json");
+const uptime = new Uptime(sharedSettings, "data/uptime_data.json");
+const status = new ApiStatus(sharedSettings);
+const libraries = new RiotAPILibraries(sharedSettings);
 
-// register commands
+// Commands controller commands
 controller.registerCommand(commandList.controller.toggle, controller.onToggle.bind(controller));
 controller.registerCommand(commandList.controller.help, controller.onHelp.bind(controller));
 
+// Botty commands
 controller.registerCommand(commandList.botty.restart, bot.onRestart.bind(bot));
 
+// API schema commands
 controller.registerCommand(commandList.apiSchema.updateSchema, apiSchema.onUpdateSchemaRequest.bind(apiSchema));
+
+// Keyfinder commands
 controller.registerCommand(commandList.keyFinder, keyFinder.onKeyList.bind(keyFinder));
 
-const versionChecker = new VersionChecker(bot.client, sharedSettings, "data/version_data.json");
+// JoinMessager commands
 controller.registerCommand(commandList.welcome, joinMessaging.onWelcome.bind(joinMessaging));
 
-const notes = new Info(bot, sharedSettings, "data/info_data.json", versionChecker);
+// Info commands
 controller.registerCommand(commandList.info.note, notes.onNote.bind(notes));
 controller.registerCommand(commandList.info.all, notes.onAll.bind(notes));
 
-const officeHours = new OfficeHours(bot.client, sharedSettings, "data/office_hours_data.json");
+// Office hours commands
 controller.registerCommand(commandList.officeHours.ask, officeHours.onAsk.bind(officeHours));
 controller.registerCommand(commandList.officeHours.ask_for, officeHours.onAskFor.bind(officeHours));
 controller.registerCommand(commandList.officeHours.open, officeHours.onOpen.bind(officeHours));
@@ -59,18 +70,18 @@ controller.registerCommand(commandList.officeHours.close, officeHours.onClose.bi
 controller.registerCommand(commandList.officeHours.question_remove, officeHours.onQuestionRemove.bind(officeHours));
 controller.registerCommand(commandList.officeHours.question_list, officeHours.onQuestionList.bind(officeHours));
 
-const react = new AutoReact(bot.client, sharedSettings, "data/thinking_data.json", "data/ignored_react_data.json");
+// Auto react commands
 controller.registerCommand(commandList.autoReact.toggle_default_thinking, react.onToggleDefault.bind(react));
 controller.registerCommand(commandList.autoReact.refresh_thinking, react.onRefreshThinking.bind(react));
 controller.registerCommand(commandList.autoReact.toggle_react, react.onToggleReact.bind(react));
 
-const uptime = new Uptime(sharedSettings, "data/uptime_data.json");
+// Uptime commands
 controller.registerCommand(commandList.uptime, uptime.onUptime.bind(uptime));
 
-const status = new ApiStatus(sharedSettings);
+// API status commands
 controller.registerCommand(commandList.apiStatus, status.onStatus.bind(status));
 
-const libraries = new RiotAPILibraries(sharedSettings);
+// Riot API libraries commands.
 controller.registerCommand(commandList.riotApiLibraries, libraries.onLibs.bind(libraries));
 
 // start bot
