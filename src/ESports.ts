0e23ba200ca4b6c2c987d05711f5954910607abc
Petter Kaspersen <petter@petterk.com>
2018-10-10 09:27:24 +0200

    Remove newline

--- src/ESports.ts
+++ src/ESports.ts
@@ -146,7 +146,6 @@ export default class ESportsAPI {
                 name: league,
                 value: output + `[More about ${league} here](${this.getUrlByLeague(league)})\n`,
             });
-
         }
 
         embed.fields.push({
