018d5a9d63d16335a6bddee57174d582d5b197aa
Querijn Heijmans <querijn@irule.at>
2019-08-24 16:29:46 +0200

    Fix join argumenting to not ignore the last argument

--- src/JoinArguments.ts
+++ src/JoinArguments.ts
@@ -1,15 +1,17 @@
 export default function joinArguments(args: string[], separators: string[], index: number = 0): string {
     let result = "";
 
-    if (args.length != separators.length) {
+    if (args.length - 1 != separators.length) {
         console.error(`Expected [ ${args.join(", ")} ] to be the same length as [ ${separators.join(", ")} ]!`);
     }
 
     let len = args.length < separators.length ? args.length : separators.length; // Just a precaution
 
-    for (let i = index; i < len; i++) {
+    for (let i = index; i < len + 1; i++) {
         result += args[i];
-        result += separators[i];
+        
+        if (i < len)
+            result += separators[i];
     }
 
     return result;
