94d60649a2a0381a22ee3fceef2d99faa04ce91a
Querijn Heijmans <querijn@irule.at>
2019-08-26 19:34:37 +0200

    Add some more rigorous cheks on joinArguments

--- src/JoinArguments.ts
+++ src/JoinArguments.ts
@@ -1,16 +1,17 @@
 export default function joinArguments(args: string[], separators: string[], index: number = 0): string {
     let result = "";
 
-    if (args.length - 1 != separators.length) {
-        console.error(`Expected [ ${args.join(", ")} ] to be the same length as [ ${separators.join(", ")} ]!`);
+    if (args.length < separators.length) {
+        console.error(`Expected [ ${args.join(", ")} ] (${args.length}) to be the same length as [ ${separators.join(", ")} ] (${separators.length})!`);
     }
 
     let len = args.length < separators.length ? args.length : separators.length; // Just a precaution
 
-    for (let i = index; i < len + 1; i++) {
-        result += args[i];
+    for (let i = index; i < len; i++) {
+        if (i < args.length)
+            result += args[i];
         
-        if (i < len)
+        if (i < separators.length)
             result += separators[i];
     }
 
